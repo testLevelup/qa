6662d5ddd0cf291dfd4c61166b3d8c62f238e94b
Gayathrivani A <gayathrivani@foxsense.io>
2024-06-24 19:24:25 +0530

    test commits

--- sample.java
+++ sample.java
@@ -3,8 +3,17 @@ public class sample {
     public static void main(String[] args) {
         
         int a=5;
-        int b=3;
+        int b=7;
         int c=a+b;
         System.out.println("c = "+ c);
+
+        if(b>a){
+           int temp=a;
+             a=b;
+             b=temp;
+             System.out.println("ascending order \n" + b +"\n"+ a);   
+
+        } 
+        
     }
 }
