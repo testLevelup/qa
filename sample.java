102ba69479bd5c238c037bd087ffae6e2fa50f0c
Gayathrivani A <gayathrivani@foxsense.io>
2024-06-24 18:58:11 +0530

    test name changes

---
+++ sample.java
@@ -0,0 +1,10 @@
+public class sample {
+
+    public static void main(String[] args) {
+        
+        int a=5;
+        int b=3;
+        int c=a+b;
+        System.out.println("c = "+ c);
+    }
+}
