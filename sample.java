431a76e3c320ef80ef78e9b19c97e3e7b20589c7
Gayathrivani A <gayathrivani@foxsense.io>
2024-06-24 19:27:41 +0530

    line change

--- sample.java
+++ sample.java
@@ -3,7 +3,7 @@ public class sample {
     public static void main(String[] args) {
         
         int a=5;
-        int b=7;
+        int b=3;
         int c=a+b;
         System.out.println("c = "+ c);
 
